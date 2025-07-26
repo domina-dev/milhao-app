a1b9e2453bd4ff0b19428784a19bf68bfb756640
vitorfiler <51916133+vitorfiler@users.noreply.github.com>
2025-07-26 14:54:18 -0300

    finalizando escopo inicial da Home Page

--- src/app/views/pages/home/home.component.ts
+++ src/app/views/pages/home/home.component.ts
@@ -7,22 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  // No seu arquivo .ts
   ranking = [
-    { nome: 'Vitin', pontuacao: 10000 },
-    { nome: 'Emy', pontuacao: 10000 },
-    { nome: 'Davi', pontuacao: 10000 },
-    { nome: 'Matheus', pontuacao: 10000 },
-    { nome: 'Junin', pontuacao: 10000 },
-    { nome: 'Pedrin', pontuacao: 10000 },
-    // etc...
+    { nome: 'Vitin', pontuacao: "1.000.000" },
+    { nome: 'Emily', pontuacao: "500.000" },
+    { nome: 'Davi', pontuacao: "200.000" },
+    { nome: 'Matheus', pontuacao: "100.000" },
+    { nome: 'Junin', pontuacao: "50.000" },
+    { nome: 'Pedrin', pontuacao: "10.000" },
+  ];
+
+  rankingComMedalhas: any[] = [];
+
+  tabelaPremiacao = [
+    { pergunta: 6, valor: "1.000.000", status: 'premio-maximo' },
+    { pergunta: 5, valor: "500.000", status: 'premio' },
+    { pergunta: 4, valor: "200.000", status: 'premio' },
+    { pergunta: 3, valor: "100.000", status: 'porto-seguro' },
+    { pergunta: 2, valor: "50.000", status: 'premio' },
+    { pergunta: 1, valor: "10.000", status: 'premio' },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log("HOME");
-
+    this.atribuirMedalhas();
   }
 
+  atribuirMedalhas() {
+    // Usa 'map' para criar a nova lista com as medalhas.
+    this.rankingComMedalhas = this.ranking.map((jogador, index) => {
+      let medalha = ''; // Padrão: sem medalha
+
+      if (index === 0) {
+        medalha = '🥇'; // Ouro para o primeiro (índice 0)
+      } else if (index === 1) {
+        medalha = '🥈'; // Prata para o segundo (índice 1)
+      } else if (index === 2) {
+        medalha = '🥉'; // Bronze para o terceiro (índice 2)
+      }
+
+      // Retorna um novo objeto combinando os dados do jogador com a nova propriedade 'medalha'.
+      return {
+        ...jogador,
+        medalha: medalha
+      };
+    });
+  }
 }
